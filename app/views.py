2b5a2f617265c7a924fc4a832dbec2c2f6013fda
Jagoda Tata <sc20jmt@leeds.ac.uk>
2022-11-21 22:00:49 +0000

    adding POST, PUT, DELETE methods

--- app/views.py
+++ app/views.py
@@ -38,7 +38,7 @@ def get_ingredients():
 # returns ingredients from specified category
 @app.route('/ingredients/<category>')
 def get_categorys_ingredients(category):
-    cat_id = IngredientCategory.query.filter_by(name=category).first().id
+    cat_id = (IngredientCategory.query.filter_by(name=category).first()).id
     ingredients = Ingredient.query.filter_by(category=cat_id).all()
 
     json_ingr = {}
@@ -59,48 +59,54 @@ def get_dishes():
     users_ingredients_ids = set() # ids of user's ingredients
     users_ingredients_names = set() # ids of user's ingredients
     json_user_ingr = request.get_json()
+    dishes_from_ingredients = {}
+    dishes_from_ingredients['dishes'] = []
 
     # getting ingredients from db specified by user
     for i in json_user_ingr:
         if not json_user_ingr[i] == '...':
-            ingredient_id = (Ingredient.query.filter_by(name=json_user_ingr[i]).first())
-            print(ingredient_id)
-            users_ingredients_ids.add(ingredient_id.id)
-            users_ingredients_names.add(ingredient_id.name)
+            ingredient = (Ingredient.query.filter_by(name=json_user_ingr[i]).first())
+            # print(ingredient)
+            users_ingredients_ids.add(ingredient.id)
+            users_ingredients_names.add(ingredient.name)
 
     # print(users_ingredients)
 
-    possible_dishes = set()
+    possible_dishes_ids = set()     # possible dishes user can make from the ingredients
     dishes = set()              # stores final set of dishes
 
-    # getting users ingredients ids
+    # getting possible dishes' ids
     for i in users_ingredients_ids:
-        # getting all dishes that contain ingredient i
+        # getting all dishes that contain ingredient i in form tuples (dish_id, ingredient_id)
         res = db.session.query(DishIngredients).filter_by(ingredient_id=i).all()
-        print(res)
+        # print(res)
+        
+        for j in res:
+            possible_dishes_ids.add(j[0])
 
-        # getting dish's id from result's tuple(dish_id, ingredient_id)
-        for d in res:
-            possible_dishes.add(d[0])
+    # checking what ingredients user is missing
+    for pd in possible_dishes_ids:
 
-    # checking if user has all ingridients for dishes in possible_dishes 
-    for pd in possible_dishes:
-        # print(pd)
-
-        # SELECT * FROM DishIngredients WHERE dish_id=pd;
+        # SELECT * FROM DishIngredients WHERE dish_id=pd[0]; -- pd[0] is id
         res = db.session.query(DishIngredients).filter_by(dish_id=pd).all()
         ingredients_needed = set()
 
-        # getting ingredients needed for specific dish
+        # getting ingredients' ids needed for specific dish
         for i in res:
             ingredients_needed.add(i[1])
 
         # checking if ingredients_needed is subset of users_ingredients
-        if ingredients_needed.issubset(users_ingredients_ids):
-            print('is subset: ', pd)
-            dishes.add((db.session.query(Dish).filter_by(id=pd).first()).name)
+        dish = {}
+        dish['name'] = ((db.session.query(Dish).filter_by(id=pd).first()).name).lower()
+        dish['missing_ingredients_ids'] = list(ingredients_needed.difference(users_ingredients_ids))
+        dish['missing_ingredients_names'] = []
 
-    return jsonify(list(dishes))
+        for mi in dish['missing_ingredients_ids']:
+            dish['missing_ingredients_names'].append(((db.session.query(Ingredient).filter_by(id=mi).first()).name).lower())
+
+        dishes_from_ingredients['dishes'].append(dish)
+
+    return jsonify(dishes_from_ingredients)
 
 @app.route('/all_dishes')
 def get_all_dishes():
@@ -113,7 +119,49 @@ def get_all_dishes():
 
 
 
-# returns dishes that contain user's ingredients 
-@app.route('/addUser')
-def add_user():
-    pass
+# adding new ingredient
+@app.route('/add_ingredient', methods=['POST'])
+def add_ingredient():
+
+    try:
+        category_id = (db.session.query(IngredientCategory).filter_by(name=request.form.get('category')).first()).id
+        ingredient = Ingredient(name=request.form.get('name'), category=category_id)
+        db.session.add(ingredient)
+        db.session.commit()
+
+        return str('You added: ' + request.form.get('name')) 
+
+    except:
+        return 'Invalid ingredient\'s data'
+
+# delete ingredient
+@app.route('/delete_ingredient/<ingredient_name>', methods=['DELETE'])
+def delete_ingredient(ingredient_name):
+    try:
+        Ingredient.query.filter_by(name=ingredient_name).delete()
+        db.session.commit()
+
+        return 'You just deleted: ' + ingredient_name
+    except:
+        return 'There is no ingredient with such a name'
+
+# update ingredient
+@app.route('/update_ingredient_category', methods=['PUT'])
+def update_ingredient():
+    try:
+        print(request.form)
+        ingr_name = request.form.get('name')
+        new_cat = request.form.get('new_cat')
+        print('new cat ', new_cat)
+        print('name ', ingr_name)
+        new_cat_id = (IngredientCategory.query.filter_by(name=new_cat).first()).id
+
+        db.session.query(Ingredient).filter_by(name=ingr_name).update({'category': new_cat_id})
+        db.session.commit()
+
+        return 'OK'
+
+    except:
+        return 'Invalid data'
+
+    
